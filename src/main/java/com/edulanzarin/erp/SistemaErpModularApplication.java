c81501e2716e19080c7dc6e0d28706dac6672625
edulanzarin <edulanzarin@outlook.com>
2026-03-06 10:16:16 -0300

    initial commit: sistema erp modular para contabilidades

---
+++ src/main/java/com/edulanzarin/erp/SistemaErpModularApplication.java
@@ -0,0 +1,13 @@
+package com.edulanzarin.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaErpModularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaErpModularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edulanzarin/erp/SistemaErpModularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edulanzarin.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaErpModularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
